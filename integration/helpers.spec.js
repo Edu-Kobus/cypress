9f654622bfb40cd3558505d22ff90aeab7b10d63
Eduardo Kobus <66337541+EduFKobus@users.noreply.github.com>
2021-06-16 21:47:08 -0300

    invoke

--- integration/helpers.spec.js
+++ integration/helpers.spec.js
@@ -27,7 +27,7 @@ describe('Helpers...', () => {
         })
     })
 
-    it.only('Its...', () => {
+    it('Its...', () => {
         const obj = { nome: 'User', idade: 20 }
         cy.wrap(obj).should('have.property','nome', 'User')
         cy.wrap(obj).its('nome').should('be.equal', 'User')
@@ -41,7 +41,19 @@ describe('Helpers...', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
 
         cy.title().its('length').should('be.equal', 20)
+    })
 
+    it.only('Invoke...', () => {
+        const getValue = () => 1;
+        const soma = (a, b) => a + b;
 
+        cy.wrap({ fn: getValue }).invoke('fn').should('be.equal', 1)
+        cy.wrap({ fn: soma }).invoke('fn', 2, 5).should('be.equal', 7)
+
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.get('#formNome').invoke('val', 'Texto via invoke')
+        cy.window().invoke('alert', 'Dá pra ver?')
+        cy.get('#resultado')
+        .invoke('html', '<input type="button", value="hacked!"/>')
     })
 })
